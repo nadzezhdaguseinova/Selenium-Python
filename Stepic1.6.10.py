a6300eb275866e4074358eeb64be37ff6f16949d
Nadezhda Guseinova <nadzezhdaguseinova@gmail.com>
2020-09-21 13:26:39 +0300

    Create Stepic1.6.10.py

---
+++ Stepic1.6.10.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    Fname = browser.find_element_by_tag_name("input")
+    Fname.send_keys("Ivan")
+    Lname = browser.find_element_by_class_name("form-control.second")
+    Lname.send_keys("Petrov")
+    email = browser.find_element_by_class_name("form-control.third")
+    email.send_keys("test@test.test")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
